2e99543ba53209d2bf13de2b3134655dcd9e37bb
anushreedganeshe-jpg <anushree.d.ganeshe@gmail.com>
2025-09-28 18:16:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('contactBtn').addEventListener('click', function() {
-  const email = document.getElementById('email');
-  if (email.style.display === 'none') {
-    email.style.display = 'block';
-  } else {
-    email.style.display = 'none';
-  }
-});
